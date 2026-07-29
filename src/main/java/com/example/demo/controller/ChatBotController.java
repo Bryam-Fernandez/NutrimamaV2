dc0d377fb3ebce95a551fbb76af2ea8a39e2c9fc
BryamCode <bryamfernandezuriarte22@gmail.com>
2026-07-28 19:58:10 -0500

    Update ChatBotController.java

--- src/main/java/com/example/demo/controller/ChatBotController.java
+++ src/main/java/com/example/demo/controller/ChatBotController.java
@@ -402,7 +402,7 @@ public class ChatBotController {
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(
-                        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
+                        "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
                                 + apiKey))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
